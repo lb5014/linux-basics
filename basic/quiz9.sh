b832646fe37c6e4860d552611399f98331aa53aa
lb5014 <lb5014@naver.com>
2024-04-03 13:11:57 +0900

    myjob

--- basic/quiz9.sh
+++ basic/quiz9.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 touch multiLine.txt
-echo 'Line 1\n''Line 2\n''Line 3'
+echo 'Line 1\nLine 2\nLine 3'
+head -3 multiLine.txt
---
+++ basic2/quiz2-4.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+mkdir myDir
+touch myFile.txt
+mv myFile.txt myDir
+rm -i myDir
+rm myFile.txt
+
+
+
