d2ff6fd2f2f4ced998191e4eaf4bd8e59ecf7cd9
lb5014 <lb5014@naver.com>
2024-04-03 13:46:18 +0900

    myjob

--- basic2/quiz2-4.sh
+++ basic2/quiz2-4.sh
@@ -2,8 +2,8 @@
 mkdir myDir
 touch myFile.txt
 mv myFile.txt myDir
-rm -i myDir
-rm myFile.txt
+rmdir myDir
+
 
 
 
---
+++ basic2/quiz2-5.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+touch notes.txt
+echo 'Line 1'
+echo 'Line 2'
+echo 'Line 3'
+cat notes.txt
+rm notes.txt
