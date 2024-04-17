1c853723c93ba7338d3819d72db68c437db0ff88
lb5014 <lb5014@naver.com>
2024-04-17 12:04:46 +0900

    myjob

--- basic3/quiz3-10.sh
+++ basic3/quiz3-10.sh
@@ -1,2 +1,18 @@
-#!/bin/sh
-ls
+#!/bin/bash
+
+target_directory="./"  # 현재 디렉토리
+file_count=0
+dir_count=0
+
+for item in *
+do
+    if [ -f "$item" ]
+    then
+        file_count=$((file_count+1))
+    elif [ -d "$item" ]
+    then
+        dir_count=$((dir_count+1))
+    fi
+done
+
+echo "파일 수: $file_count, 디렉토리 수: $dir_count"
